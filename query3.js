b59d120c1aa0a828686db42055f5c043b70e044f
Qingyao Zhu <qingyaoz@caen-vnc-mi15.engin.umich.edu>
2024-03-24 17:25:27 -0400

    finished, passed local test

--- query3.js
+++ query3.js
@@ -10,6 +10,17 @@ function cities_table(dbname) {
     db = db.getSiblingDB(dbname);
 
     // TODO: implement cities collection here
-
+    db.users.aggregate([
+        {
+            $group: {
+                _id: "$current.city", // 按照当前居住城市分组
+                users: { $addToSet: "$user_id" } // 将用户id添加到数组中，并确保唯一性
+            }
+        },
+        {
+            $out: "cities" // 将结果输出到cities集合中
+        }
+    ]);
+    
     return;
 }
